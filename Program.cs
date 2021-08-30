c9be5df7914f592177392ac89a038b276a988acc
Ankesh <Ankesh@LENOVO>
2021-08-30 20:57:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+namespace BinaryReadersWriters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fileName = "AppSettings.dat";
+
+            static void Main()
+            {
+                WriteDefaultValues();
+                DisplayValues();
+            }
+
+            static void WriteDefaultValues()
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+                {
+                    writer.Write(1.250F);
+                    writer.Write(@"c:\Temp");
+                    writer.Write(10);
+                    writer.Write(true);
+                }
+            }
+
+             static void DisplayValues()
+            {
+                float aspectRatio;
+                string tempDirectory;
+                int autoSaveTime;
+                bool showStatusBar;
+
+                if (File.Exists(fileName))
+                {
+                    using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+                    {
+                        aspectRatio = reader.ReadSingle();
+                        tempDirectory = reader.ReadString();
+                        autoSaveTime = reader.ReadInt32();
+                        showStatusBar = reader.ReadBoolean();
+                    }
+
+                    Console.WriteLine("Aspect ratio set to: " + aspectRatio);
+                    Console.WriteLine("Temp directory is: " + tempDirectory);
+                    Console.WriteLine("Auto save time set to: " + autoSaveTime);
+                    Console.WriteLine("Show status bar: " + showStatusBar);
+                }
+            }
+        }
+    }
+}
